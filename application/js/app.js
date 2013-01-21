98564cbe982a34542c59bb52ecb5f865ba16dd4c
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2013-01-21 16:02:43 -0600

    Adding basic application view and hooking up tabletop.

--- application/js/app.js
+++ application/js/app.js
@@ -4,23 +4,52 @@
 var LTApplication = Backbone.Router.extend({
   routes: {
     'bill/:bill': 'bill',
-    '*default': 'default'
+    '*defaultR': 'defaultR'
   },
 
   initialize: function(options) {
     this.options = options;
+    
+    // Bind to help with some event callbacks
+    _.bindAll(this);
+    
     // Data cache is used to store loaded models
     // as this data does not change that often
     this.dataCache = {};
     
+    // Main view for application
+    this.mainView = new LSApplicationView(options);
+    this.mainView.loading();
+    
+    // Get data from spreadsheets
+    this.tabletop = Tabletop.init({
+      key: this.options.billsSheet,
+      simpleSheet: true,
+      postProcess: this.parseEBills,
+      callback: this.loadEBills,
+      callbackContext: this
+    });
+  },
+  
+  // Function to call when bill data is loaded
+  loadEBills: function(data, tabletop) {
+    
+    // Start handling routing and history
     Backbone.history.start();
   },
+  
+  // Function to parse out any data from the spreadsheet
+  // for the bills
+  parseEBills: function(row) {
+    return row; 
+  },
 
-  default: function() {
+  // Default route
+  defaultR: function() {
     
   },
   
-  // Bill page
+  // Bill route
   bill: function(bill) {
     this.loadModel('OSBillModel', 'bill_id', bill, function(bill, data, xhr) {
       
---
+++ application/js/views.js
@@ -0,0 +1,18 @@
+/**
+ * Views for the Legislator Tracker app
+ */
+var LSApplicationView = Backbone.View.extend({
+  templates: {
+    loading: $('#template-loading').html()
+  },
+  
+  initialize: function(options) {
+    // Add class to ensure our styling does
+    // not mess with other stuff
+    this.$el.addClass('ls');
+  },
+
+  loading: function() {
+    this.$el.html(_.template(this.templates.loading, {}));
+  }
+});
