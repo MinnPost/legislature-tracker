3ec007b2981683db21918925f8d8c3c8fbfd882f
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2013-01-24 12:04:59 -0600

    Better comments for AWS keys.

--- grunt.js
+++ grunt.js
@@ -66,10 +66,12 @@ module.exports = function(grunt) {
     },
     uglify: {},
     s3: {
+      // This is specific to MinnPost
+      //
       // These are assumed to be environment variables
       // See https://npmjs.org/package/grunt-s3
-      //
-      // This is specific to MinnPost
+      //key: 'YOUR KEY',
+      //secret: 'YOUR SECRET',
       bucket: 'data.minnpost',
       access: 'public-read',
       upload: [
