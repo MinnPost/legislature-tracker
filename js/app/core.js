d89268a5da4d66d6a389c2185ed10c405952258c
Alan Palazzolo <apalazzolo@minnpost.com>
2013-05-02 18:47:23 -0400

    Fixing bug that overwrites companino and conference bills.

--- js/app/core.js
+++ js/app/core.js
@@ -179,7 +179,6 @@ else {
         LT.log('Invalid primary bill number "' + row.bill + '" for row ' + row.rowNumber + ', see documentation.');
       }
       
-      row.bill_companion = undefined;
       if (row.bill_companion && LT.parse.validateBillNumber(row.bill_companion)) {
         row.bill_companion = LT.utils.getModel('OSBillModel', 'bill_id', { bill_id: row.bill_companion });
       }
@@ -187,7 +186,6 @@ else {
         LT.log('Invalid companion bill number "' + row.bill_companion + '" for row ' + row.rowNumber + ', see documentation.');
       }
       
-      row.bill_conference = undefined;
       if (row.bill_conference && LT.parse.validateBillNumber(row.bill_conference)) {
         row.bill_conference = LT.utils.getModel('OSBillModel', 'bill_id', { bill_id: row.bill_conference });
       }
@@ -298,6 +296,8 @@ else {
         delete row[input];
       }
     });
+    
+    return row;
   };
   
   // Default options
